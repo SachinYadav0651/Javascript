f6a3688b8a0d9ae65a28bea4543a13fe745491dd
SachinYadav0651 <syadav0651@gmail.com>
2025-03-13 06:45:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; //  treat all JS code as newer version
+
+// alert(3 + 3)   // we are using nodejs, not browser
+
+console.log(3 +
+    3)           // Code readability should be high
+
+console.log("Sachin")
+
+let name = "Sachin"
+let age = 26
+let isLoggedIn = true
+let char = "$"
+let test;
+let check = null
+let abc = ""
+
+// number => 2 to power 53
+// big int
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// Object
+
+console.log(typeof "Sachin", typeof age, typeof undefined, typeof null, typeof abc, typeof check, typeof test)
+
+// typeof Null = object
+// typeof undefined = undefined
+
+
+
+
+
