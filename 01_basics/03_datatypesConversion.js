d5a9430d9b29cdb9579c326479524e7ad2090a3c
SachinYadav0651 <syadav0651@gmail.com>
2025-03-14 06:17:44 +0000

    datatypes conversion

---
+++ 01_basics/03_datatypesConversion.js
@@ -0,0 +1,133 @@
+/*
+let score = "Sachin"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);         // NaN = Not a number
+
+score = "99"
+valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);         // 99
+
+score = "99abcd"
+valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);         // NaN
+
+score = null
+valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);         // 0
+
+score = undefined
+valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);         // NaN
+
+score = true
+valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);         // 1
+
+score = false
+valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);         //0
+
+
+
+let isloggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);           // true
+
+isloggedIn = ""
+
+booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);           // false
+
+isloggedIn = "Sachin"
+
+booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);           // true
+
+isloggedIn = null
+
+booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);           // false 
+
+isloggedIn = undefined
+
+booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);           // false
+
+isloggedIn = 0
+
+booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);           // false
+
+isloggedIn = 230
+
+booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);           // true
+
+*/
+
+let someNbr = 123
+
+let stringSomeNbr = String(someNbr)
+console.log(typeof stringSomeNbr);
+console.log(stringSomeNbr);       // 123
+
+someNbr = true
+
+stringSomeNbr = String(someNbr)
+console.log(typeof stringSomeNbr);
+console.log(stringSomeNbr);       // true
+
+someNbr = false
+
+stringSomeNbr = String(someNbr)
+console.log(typeof stringSomeNbr);
+console.log(stringSomeNbr);      // false
+
+someNbr = null
+
+stringSomeNbr = String(someNbr)
+console.log(typeof stringSomeNbr);
+console.log(stringSomeNbr);     // null
+
+someNbr = undefined
+
+stringSomeNbr = String(someNbr)
+console.log(typeof stringSomeNbr);
+console.log(stringSomeNbr);      // undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
